d9e2cac7cf496bf5b8fa001cf28310e797586e17
monitech1 <mon22014@byui.edu>
2023-09-24 18:30:15 +0000

    welcome to the pprogram added

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,54 @@
 using System;
 
-class Program
+namespace Prep5
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep4 World!");
+        static void Main(string[] args)
+        {
+            // Display welcome message
+            DisplayWelcome();
+
+            // Prompt user for their name
+            string userName = PromptUserName();
+
+            // Prompt user for their favorite number
+            int userNumber = PromptUserNumber();
+
+            // Calculate the square of the number
+            int squaredNumber = SquareNumber(userNumber);
+
+            // Display the result
+            DisplayResult(userName, squaredNumber);
+
+            Console.ReadLine();
+        }
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        static void DisplayResult(string userName, int squaredNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+        }
     }
 }
