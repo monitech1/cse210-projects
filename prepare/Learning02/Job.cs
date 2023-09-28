96414e67c2fdb62e4bb2b1725ac5e55a4444311b
monitech1 <mon22014@byui.edu>
2023-09-28 21:30:52 +0000

    jobs

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Job
+{
+    public string Company { get; set; }
+    public string JobTitle { get; set; }
+    public int StartYear { get; set; }
+    public int EndYear { get; set; }
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{JobTitle} ({Company}) {StartYear}-{EndYear}");
+    }
+}
