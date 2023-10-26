33d57e389d3285e2b4c1a41465b4da1eabd1105d
monitech1 <mon22014@byui.edu>
2023-10-26 18:34:14 +0000

    W07 Prepare: Learning Activity

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,27 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    // Getter methods for private member variables
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+}
