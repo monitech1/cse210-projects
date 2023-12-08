bcfa79747e20784f13469c6722294513efaf770a
Monitech1 <mon22014@byui.edu>
2023-12-08 08:36:24 +0000

    Completion of  Foundation 3

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,53 @@
 using System;
 
-internal class Program
+class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address = new Address("St Monigan de Ghana", "Lome", "Lome", "Accra");
+
+        Lecture lecture = new Lecture("Literary Reading: 'A Perfect Short Story'", "An evening of literary exploration with Valeria Alcala", "November 29, 2023", "9 AM", address, "Valeria Alcala", 120);
+        Console.WriteLine();
+
+        Console.WriteLine("Event Number: 1");
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateStandard());
+
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateDetailedLecture());
+
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateShortLecture());
+
+        Address addressR = new Address("Owerri", "Los Olivos", "Lome", "Accra");
+
+        Reception reception = new Reception("Devotional for Young Single Adults", "Face to face with Elder Quentin L. Cook for YSA", "August 18, 2023", "9 PM", addressR, "devotional@youngadult.com");
+        Console.WriteLine();
+
+        Console.WriteLine("Event Number: 2");
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateStandard());
+
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateDetailedReception());
+
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateShortReception());
+
+        Address addressOG = new Address("Plaza de Armas", "Cusco", "Cusco", "Peru");
+
+        OutdoorGathering outdoorGathering = new OutdoorGathering("Festival Inti Raymi", "The Celebration of the Sun", "September 16, 2023", "9 PM", addressOG, "sunny");
+        Console.WriteLine();
+
+        Console.WriteLine("Event Number: 3");
+
+        Console.WriteLine();
+        Console.WriteLine(outdoorGathering.GenerateStandard());
+
+        Console.WriteLine();
+        Console.WriteLine(outdoorGathering.GenerateDetailedOutdoorGathering());
+
+        Console.WriteLine();
+        Console.WriteLine(outdoorGathering.GenerateShortOutdoorGathering());
     }
 }
